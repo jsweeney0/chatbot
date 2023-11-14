294e2f5036a98335c5dd5a6f599fa3437622636d
Jade Sweeney <jadexsweeney@gmail.com>
2023-11-14 12:08:38 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from hugchat import hugchat
 from hugchat.login import Login
 
-# App Title
+# App title
 st.set_page_config(page_title="🤗💬 HugChat")
 
 # Hugging Face Credentials
@@ -20,40 +20,39 @@ with st.sidebar:
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
     st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-an-llm-powered-chatbot-with-streamlit/)!')
-
+    
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
-  st.session_state.messages = [{"role": "assisant", "content": "How may I help you?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
 
 # Display chat messages
 for message in st.session_state.messages:
-  with st.chat_message(message["role"]):
-                       st.write(message["content"])
-
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
 # Function for generating LLM response
 def generate_response(prompt_input, email, passwd):
-  # Hugging face login
-  sign = Login(email,passwd)
-  cookies = sign.login()
-  # Create chatbot
-  chatbot = hugchat.ChatBot(cookies = cookies.get_dict())
-  return chatbot.chat(prompt_input)
+    # Hugging Face Login
+    sign = Login(email, passwd)
+    cookies = sign.login()
+    # Create ChatBot                        
+    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    return chatbot.chat(prompt_input)
 
-# User provided prompt
-if prompt := st.chat_input(disabled = not (hf_email and hf_pass)):
-  st.session_state.messages.append({"role": "user", "content": prompt})
-  with st.chat_message("user"):
-    st.write(prompt)
+# User-provided prompt
+if prompt := st.chat_input(disabled=not (hf_email and hf_pass)):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
 
-# Generate a new response if last message is not from the assistant
+# Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
-  with st.chat_message("assistant"):
-    with st.spinner("Thinking..."):
-      response = generate_response(prompt, hf_email, hf_pass)
-      st.write(response)
-  message = {"role": "assistant", "content": response}
-  st.session_state.messages.append(message)
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_response(prompt, hf_email, hf_pass) 
+            st.write(response) 
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
 
 
       
